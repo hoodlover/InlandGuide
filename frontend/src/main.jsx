becdc54a89adb302f6a99eae15e28f1bc33e4380
Hoodlove <lance.climb@gmail.com>
2026-08-28 15:16:59 -0400

    Add temporary Inland Guide access screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AccessBlock from './AccessBlock.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Temporary access block. Restore App.jsx here to reopen the guide. */}
+    <AccessBlock />
   </React.StrictMode>,
 )
