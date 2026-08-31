5d497982309c757d8db9817ce0780efe256c95f8
Hoodlove <lance.climb@gmail.com>
2026-08-31 08:30:18 -0400

    Revert "Add temporary Inland Guide access screen"
    This reverts commit becdc54a89adb302f6a99eae15e28f1bc33e4380.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AccessBlock from './AccessBlock.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Temporary access block. Restore App.jsx here to reopen the guide. */}
-    <AccessBlock />
+    <App />
   </React.StrictMode>,
 )
